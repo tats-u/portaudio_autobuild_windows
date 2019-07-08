3a5b491c73fa4e7690bfabd1fc05402cfc62f4fb
Tatsunori Uchino <tats.u@live.jp>
2019-07-08 17:42:46 +0900

    Make it possible to change build root

--- build_portaudio_win.ps1
+++ build_portaudio_win.ps1
@@ -33,13 +33,15 @@ Build PortAudio with support of MME (Slowest)
 Don't build.  Build it manually for example using Visual Studio.
 .PARAMETER DebugBuild
 Build PortAudio with Debug configuration.
+.PARAMETER BuildRoot
+Build root directory.  Library will be created at $BuildRoot\{Debug,Release}\.
 
 .LINK
 https://github.com/tats-u/portaudio_autobuild_windows
 #>
 
 [cmdletbinding()]
-Param([switch]$NoASIO, [switch]$NoWASAPI, [switch]$WDM, [switch]$MME, [switch]$DownloadOnly, [switch]$DebugBuild)
+Param([switch]$NoASIO, [switch]$NoWASAPI, [switch]$WDM, [switch]$MME, [switch]$DownloadOnly, [switch]$DebugBuild, [string]$BuildRoot = "$PSScriptRoot\portaudio\build")
 
 $ErrorActionPreference = "Stop"
 
@@ -66,24 +68,33 @@ try {
     }
   }
 
-  Set-Location portaudio
-  # Fix typo preventing us from enabling WASAPI
-  # See: https://app.assembla.com/spaces/portaudio/tickets/261/details
-  $RootCMakeListsContent = Get-Content -Encoding UTF8 -Raw CMakeLists.txt
-  if ($RootCMakeListsContent -match "`n *IF\(MSVS\) *`n") {
-    Write-Verbose "Fixing bug in CMakeLists.txt in PortAudio."
-    [IO.File]::WriteAllText("$PWD\CMakeLists.txt", $RootCMakeListsContent.Replace("IF(MSVS)", "IF(MSVC)"))
-  }
+  Push-Location
+  try {
+    Set-Location portaudio
+    # Fix typo preventing us from enabling WASAPI
+    # See: https://app.assembla.com/spaces/portaudio/tickets/261/details
+    $RootCMakeListsContent = Get-Content -Encoding UTF8 -Raw CMakeLists.txt
+    if ($RootCMakeListsContent -match "`n *IF\(MSVS\) *`n") {
+      Write-Verbose "Fixing bug in CMakeLists.txt in PortAudio."
+      [IO.File]::WriteAllText("$PWD\CMakeLists.txt", $RootCMakeListsContent.Replace("IF(MSVS)", "IF(MSVC)"))
+    }
 
-  if (-not $NoASIO -and (-not (Test-Path src\hostapi\asio\ASIOSDK))) {
-    Write-Verbose "Downloading ASIO SDK."
-    Set-Location src\hostapi\asio
-    Invoke-WebRequest https://www.steinberg.net/asiosdk -OutFile asiosdk.zip
-    Expand-Archive -Path .\asiosdk.zip -DestinationPath .
-    Remove-Item .\asiosdk.zip
-    # The last \ is to specify a folder.
-    # Just renaming ASIO SDK folder.
-    Move-Item asiosdk_*.*.*_*\ ASIOSDK
+    if (-not $NoASIO -and (-not (Test-Path src\hostapi\asio\ASIOSDK))) {
+      Write-Verbose "Downloading ASIO SDK."
+      Set-Location src\hostapi\asio
+      Invoke-WebRequest https://www.steinberg.net/asiosdk -OutFile asiosdk.zip
+      Expand-Archive -Path .\asiosdk.zip -DestinationPath .
+      Remove-Item .\asiosdk.zip
+      # The last \ is to specify a folder.
+      # Just renaming ASIO SDK folder.
+      Move-Item asiosdk_*.*.*_*\ ASIOSDK
+    }
+  }
+  catch {
+    throw
+  }
+  finally {
+    Pop-Location
   }
 
   if (-not $DownloadOnly) {
@@ -95,13 +106,22 @@ try {
       Write-Warning "Command Import-VisualStudioEnvironment is not installed.  It can be installed by:`nInstall-Module -Name WintellectPowerShell -Scope CurrentUser"
     }
     $BuildType = if ($DebugBuild) { "Debug" } else { "Release" }
-    Set-Location $PSScriptRoot\portaudio\build
-    Write-Verbose "Configuring with CMake..."
-    Write-Verbose "Build Type: $BuildType / ASIO: $(-not $NoASIO) / WASAPI: $(-not $NoWASAPI) / WDM: $WDM / MME: $MME"
-    cmake .. -G "Visual Studio $((Get-Command msbuild).Version.Major)" -A x64 "-DPA_USE_ASIO=$(if($NoASIO) { 'OFF' } else { 'ON' })" "-DASIOSDK_ROOT_DIR=..\src\hostapi\asio\ASIOSDK" "-DPA_USE_WMME=$(if($MME) { 'ON' } else { 'OFF' })" "-DPA_USE_WASAPI=$(if($NoWASAPI) { 'OFF' } else { 'ON' })" "-DPA_USE_WDMKS=$(if($WDM) { 'ON' } else { 'OFF' })" -DPA_USE_DS=OFF "-DCMAKE_BUILD_TYPE=$BuildType"
-    Write-Verbose "Build starting."
-    msbuild portaudio.sln /t:build "/p:Configuration=$BuildType" /v:m /nologo
-    Write-Output "PortAudio was successfully built.  Use $PWD\$BuildType\portaudio_x64.{dll,lib}."
+    Push-Location
+    try {
+      Set-Location $BuildRoot
+      Write-Verbose "Configuring with CMake..."
+      Write-Verbose "Build Type: $BuildType / ASIO: $(-not $NoASIO) / WASAPI: $(-not $NoWASAPI) / WDM: $WDM / MME: $MME"
+      cmake .. -G "Visual Studio $((Get-Command msbuild).Version.Major)" -A x64 "-DPA_USE_ASIO=$(if($NoASIO) { 'OFF' } else { 'ON' })" "-DASIOSDK_ROOT_DIR=..\src\hostapi\asio\ASIOSDK" "-DPA_USE_WMME=$(if($MME) { 'ON' } else { 'OFF' })" "-DPA_USE_WASAPI=$(if($NoWASAPI) { 'OFF' } else { 'ON' })" "-DPA_USE_WDMKS=$(if($WDM) { 'ON' } else { 'OFF' })" -DPA_USE_DS=OFF "-DCMAKE_BUILD_TYPE=$BuildType"
+      Write-Verbose "Build starting."
+      msbuild portaudio.sln /t:build "/p:Configuration=$BuildType" /v:m /nologo
+      Write-Output "PortAudio was successfully built.  Use $PWD\$BuildType\portaudio_x64.{dll,lib}."
+    }
+    catch {
+      throw
+    }
+    finally {
+      Pop-Location
+    }
   }
 
 }
